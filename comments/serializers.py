9fe67b3f67c96b704fba814389a73e0e94b89703
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-10 15:45:07 +0000

    add comments url and path

--- comments/serializers.py
+++ comments/serializers.py
@@ -23,3 +23,11 @@ class CommentSerializer(serializers.ModelSerializer):
             'profile_id', 'profile_image', 'post',
             'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
+    post = serializers.ReadOnlyField(source='post.id')
